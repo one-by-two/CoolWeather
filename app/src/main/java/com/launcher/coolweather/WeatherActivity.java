60137e540ee4f21c860740e0cf30c4ce3466a3c7
LBW <271328107@qq.com>
2018-12-18 17:20:42 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/launcher/coolweather/WeatherActivity.java
+++ app/src/main/java/com/launcher/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.launcher.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.launcher.coolweather.gson.Forecast;
 import com.launcher.coolweather.gson.Weather;
+import com.launcher.coolweather.service.AutoUpdateService;
 import com.launcher.coolweather.util.HttpUtil;
 import com.launcher.coolweather.util.Utility;
 
@@ -219,5 +221,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
